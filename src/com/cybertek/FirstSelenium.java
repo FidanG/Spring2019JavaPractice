bbb10d3c6d4e62379dbd53d5bffc58f81e71760b
FidanG <fidang87@gmail.com>
2019-06-16 16:22:11 -0400

    first commit

---
+++ src/com/cybertek/FirstSelenium.java
@@ -0,0 +1,20 @@
+package com.cybertek;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstSelenium {
+
+    public static void main(String[] args) {
+
+
+        System.setProperty("webdriver.gecko.driver", "/Users/fidangasimova/Documents/selenium dependencies/drivers/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+
+driver.get("https://google.com");
+
+
+
+    }
+}
